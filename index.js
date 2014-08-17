ce866b463491909b5982848fbf9c88f4fd90916a
Vincent Moore <catalystlive@hotmail.com>
2014-08-17 18:33:47 +0000

    delete test scripts and add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var oop = require('oop-module');
+var MangaEden = oop.class('./scripts/scrape/MangaEden');
+var KissManga = oop.class('./scripts/scrape/KissManga');
+
+exports.MangaEden = MangaEden;
+exports.KissManga = KissManga;
