f577123e13fd3d9f08d5801e7126b4dac6ce53a6
Vincent Moore <catalystlive@hotmail.com>
2014-10-10 13:10:14 +0000

    add mangainfo to mangapanda

--- scripts/scrape/MangaPanda.js
+++ scripts/scrape/MangaPanda.js
@@ -11,7 +11,7 @@ MangaPanda.prototype = {
     {
         var networkOptions = {
             url: this.baseUrl + '/alphabetical',
-            manga: 'directory'
+            mangaName: 'directory'
         };
         
         network.request({
@@ -27,6 +27,7 @@ MangaPanda.prototype = {
                 var $ = DOM;
                 var directory = [];
                 
+                //Returns link with prepended directories and top level domains - /369/ziggy-ziggy.html
                 $('.series_col ul a').each(function(){
                     var mangaName = $(this).attr("href");
                     mangaName = mangaName.replace(/\/?[0-9]*\//gi,'').replace(/.html/gi,'');
@@ -37,9 +38,83 @@ MangaPanda.prototype = {
         });
     },
     
-    mangaInfo: function( args )
+    mangaInfo: function( args, callback )
     {
+        /*
+            title
+            description
+            coverUrl
+            release
+            artist
+            author
+            genres []
+            scanOrigin
+            numOfChapters
+            status
+            chapters [ number, title, pages [empty] ]
+        */
         var mangaName = args.mangaName;
+        var requestUrl = this.baseUrl + '/' + mangaName;
+        var networkOptions = {
+            url: requestUrl,
+            mangaName: mangaName
+        }
+        
+        network.request({
+            options: networkOptions,
+            callback: function( err, DOM ){
+                
+                var $ = DOM;
+            
+                var title = $('.aname').text();
+                var description = $('#readmangasum p').text();
+                var coverUrl = $('#mangaimg img').attr('src');
+                var release = $('#mangaproperties table tr').eq(2).children('td').eq(1).text();
+                var artist = $('#mangaproperties table tr').eq(5).children('td').eq(1).text();
+                var author = $('#mangaproperties table tr').eq(4).children('td').eq(1).text();
+                var genres = [];
+                var scanOrigin = "MangaPanda";
+                var numOfChapters = $('#latestchapters ul li a').eq(0).text().replace(title,"").trim();
+                var status = $('#mangaproperties table tr').eq(3).children('td').eq(1).text();
+                var chapters = [];
+                
+                $('#mangaproperties table tr').eq(7).children('td').eq(1).children('a').each(function(){
+                    var genre = $(this).children('span').text();
+                    genres.push(genre);
+                });
+                
+                $('#listing tr').each(function( index, element ){
+                    if( index === 0 )
+                    {
+                        return;
+                    }
+                    //Returns date chapter number and title - 'Naruto 1 : All you have!! 07/23/2014 (tons of spacing)
+                    var rawTitle = $(this).text();
+                    rawTitle = rawTitle.trim();
+                    rawTitle = rawTitle.replace(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/gi,'').trim();
+                    
+                    var number = rawTitle.match(/[0-9]+/)[0];
+                    var chapterTitle = rawTitle.replace(title + ' ' + number + ' : ','');
+                    chapters.push({ number: number, title: chapterTitle, pages: [] });
+                });
+                
+                var mangaObject = {
+                    title: title,
+                    description: description,
+                    coverUrl: coverUrl,
+                    release: release,
+                    artist: artist,
+                    author: author,
+                    genres: genres,
+                    scanOrigin: scanOrigin,
+                    numOfChapters: numOfChapters,
+                    status: status,
+                    chapers: chapters
+                };
+                
+                callback( null, mangaObject );
+            }
+        });
         
     },
     
